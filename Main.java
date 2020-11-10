b66898e54e643cf32ae04d51cf6a2b8fc8ceacd9
olu360view <72469010+olu360view@users.noreply.github.com>
2020-11-09 17:33:22 -0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*
+
+/*class Main {
+  public static void main(String[]args) {
+    int p=5;
+    int a[]=new int[5];
+    a[0]=10;
+    a[1]=20;
+    a[3]=40;
+    a[4]=50;
+    
+    a[3]+=20;
+    p+=20;
+    
+    //System.out.print(a[4]);
+    
+    for(int i=0,i<a.length; i
+    ++){
+    
+    System.out.print1n("The "+i+"value of a has the number 30");
+    */
+    ArrayList<integer> numBax=new ArrayList;
